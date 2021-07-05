7e0b10de3a2004d22729296053e1b06fae78622f
dukza <dyha7648@gmail.com>
2021-07-05 13:48:56 +0900

    test

--- src/component/Buttons/Button.stories.js
+++ src/component/Buttons/Button.stories.js
@@ -7,7 +7,7 @@ import Button from './Button';
 
 storiesOf("Button", module)
     .addDecorator(withDesign)
-    .add("primary",()=><Button>Primary</Button>,{
+    .add("primary",()=><Button>Primary1</Button>,{
         design: {
             type: 'figma',
             url:
