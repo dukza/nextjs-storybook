298bc38086b4b797f553e33a6319bd507d0c4023
dukyeon ha <dyha@fssuniverse.com>
2021-08-25 22:22:36 +0900

    test

--- next.config.js
+++ next.config.js
@@ -2,6 +2,5 @@ module.exports = {
   images: {
     loader: "imgix",
     path: "https://dukza.github.io/nextjs-storybook/",
- },
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://dukza.github.io/nextjs-storybook/' : ''
+ }
 }
